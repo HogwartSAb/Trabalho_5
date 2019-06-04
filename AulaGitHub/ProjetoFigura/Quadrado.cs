b0c135eb523c977d35efd348089a889d760f9c9d
Unifenas Laboratorios <unifenas.laboratorios@outlook.com>
2019-06-03 22:02:05 -0300

    Qualquer coisa

---
+++ AulaGitHub/ProjetoFigura/Quadrado.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoFigura
+{
+    class Quadrado
+    {
+        private double lado;
+
+        public double Lado {set => lado = value; }
+
+        public double Area()
+        {
+            return (lado*lado);
+        }
+    }
+}
---
+++ AulaGitHub/ProjetoFigura/Triangulo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoFigura
+{
+    class Triangulo
+    {
+        private double alturaT;
+        private double baseT;
+
+        public double AlturaT
+        {
+            set = alturaT = value; }
+        public double BaseT
+        {
+            set = baseT = value; }
+
+        public double Area()
+        {
+            return (baseT  alturaT)  2;
+        }
+    }
+}
